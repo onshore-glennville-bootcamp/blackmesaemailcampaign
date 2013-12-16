042c6626a77223d64e60835848e49c01ba962fa6
barrattac@github.com <barrattac@hotmail.com>
2013-12-16 08:30:05 -0500

    13.12.16.1, Add Subscriber Form

---
+++ BlackMesaEmailCampaign/Controllers/SubscribersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlackMesaEmailCampaign.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(SubscribersController subscribers)
+        {
+            return RedirectToRoute("Index", "Home");
+        }
+	}
+}
