a5103b77d9b246e52cda336897f67208efdaf8b7
markkalgren <mark.kalgren@gmail.com>
2013-12-31 11:11:26 -0500

    Upload/read functionality working, needs polished

--- BlackMesaEmailCampaign/Controllers/SubscribersController.cs
+++ BlackMesaEmailCampaign/Controllers/SubscribersController.cs
@@ -66,21 +66,36 @@ namespace BlackMesaEmailCampaign.Controllers
         public ActionResult AddFromFile(HttpPostedFileBase file)
         {
             UserServices userServices = new UserServices();
+            bool uploaded = true;
             // Verify that the user selected a file
             if (file != null && file.ContentLength > 0)
             {
-                // extract only the fielname
-                var fileName = Path.GetFullPath(file.FileName);
-                if (userServices.CheckCSV(fileName))
+                // Extract and check file path
+                //var fileName = Path.GetFullPath(file.FileName);
+                //if (userServices.CheckCSV(fileName))
+                //{
+                //    userServices.SeparateCSV(fileName);
+                //}
+                string fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                string path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+                if (userServices.CheckCSV(path))
                 {
-                    userServices.SeparateCSV(fileName);
+                    userServices.SeparateCSV(path);
                 }
-                //// store the file inside ~/App_Data/uploads folder
-                //var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                //file.SaveAs(path);
             }
             // redirect back to the index action to show the form once again
-            return RedirectToAction("AddFromFile");  
+            if (uploaded == true)
+            {
+                ViewBag.ErrorMessage = "Subscribers uploaded.";
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Upload unsuccessfull";
+                return View();
+            }
         }
         public ActionResult ViewSubscribers()
         {
