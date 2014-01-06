b2461a824629e565efad8a157d058421b371023d
barrattac@github.com <barrattac@hotmail.com>
2014-01-06 08:58:17 -0500

    Added Classes and Controller for Groups

---
+++ BlackMesaEmailCampaign/Controllers/SubscriberGroupsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlackMesaEmailCampaign.Controllers
+{
+    public class SubscriberGroupsController : Controller
+    {
+        //
+        // GET: /SubscriberGroups/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
